97b0918f230d77fb913154d9fc010402fd184f31
AmatorTeur <jjj.aaa2005@mail.ru>
2024-03-23 21:48:26 +0300

    2.1
    NavMesh

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform CameraAxisTransform;
+    public float minAngle = 1;
+    public float maxAngle = 10;
+    public float _rotationSpeed = 1;
+
+    void Update()
+    {
+        var newAngleY = transform.localEulerAngles.y + Time.deltaTime * _rotationSpeed * Input.GetAxis("Mouse X");
+        transform.localEulerAngles = new Vector3(0, newAngleY, 0);
+        var newAngleX = CameraAxisTransform.localEulerAngles.x + Time.deltaTime * _rotationSpeed * -Input.GetAxis("Mouse Y");
+        if (newAngleX > 180) newAngleX -= 360;
+        newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+        CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
+    }
+}
